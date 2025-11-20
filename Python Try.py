7352332ae8c9ca0feded4a8a1ae91e01f514652d
KricoFR <kristophex5@gmail.com>
2025-11-20 15:14:17 -0700

    Create Python Try.py

---
+++ Python Try.py
@@ -0,0 +1,23 @@
+#=============Probar el "TRY_EXCEPT_FINALLY" en python=============
+#   -El try se usa para seccionar una parte del codigo para correrlo
+#       a expetativas de cometer errores
+#   -Se usa el "try:" para empezar el codigo justo despues de aniaden
+#       instruccciones.
+#   -Justo despues se aniaden exepciones como "except:" para poder 
+#       encapsular las opciones de los errores conocidos.
+#   -El "else:" se usa para englobar todas los errores no conocidos
+#       que podrian surgir en la ejecucucion del codigo en cuestion
+#   -El "finally:" es el comando que solo se coloca al final y se muestra#       independientemente de si el "try:" dio un error o no, osea, una
+#       instruccion o un comando al momento que el programa finalice el
+#       "try:".
+
+
+
+
+
+
+
+
+
+
+
