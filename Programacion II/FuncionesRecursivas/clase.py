92ce456c5eb348ef4c369dbc93cf710b7bb2b220
KricoFR <kristophex5@gmail.com>
2026-02-25 10:10:06 -0700

    Update de ficheros en FuncionesRecursivas adicion de instrucciones para actividad 2

---
+++ Programacion II/FuncionesRecursivas/clase.py
@@ -0,0 +1,39 @@
+
+# atributos
+# marca
+# Modelo
+# anio
+
+def darBienvenida():
+   print("""Hola nuevo usuario nos vemos de nuevo
+entonces que quieres hacer?
+[1] Adjuntar un nuevo veiculo
+[2] Cambiar un vehiculo
+[3] Solicitar un dato
+[4] Salir
+""")
+class Auto:
+   def __int__(self,):
+      self.catalogo = []
+
+   def menu(self):
+      value = int(input())
+      while True:
+         match value:
+            case 1:
+               menu2()
+            case 2:
+               pass
+            case 3:
+               pass
+
+   def menu2(self):
+      print
+   def modelo(self):
+      catalogo[modelo] = input()
+
+   def marca(self):
+      catalogo.update
+
+   def anio(self):
+      anio.update
---
+++ Programacion II/FuncionesRecursivas/main.py
@@ -0,0 +1,11 @@
+# crear una lista ordenada de autos proporcionada por el usuario\n
+# Los datos del auto se van a meter en diccionarios especificos
+# crear una funcion para que add a el diccionario los datos porporcionados del usuario
+from clase import Auto, darBienvenida
+
+catalogo=[]
+
+darBienvenida()
+
+
+
