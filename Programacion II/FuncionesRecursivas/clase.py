bb6b739ab6f3dc70224a4966c76a18907ce6d026
KricoFR <kristophex5@gmail.com>
2026-02-26 07:55:17 -0700

    actualizacion de funciones FuncionesRecursivas

--- Programacion II/FuncionesRecursivas/clase.py
+++ Programacion II/FuncionesRecursivas/clase.py
@@ -1,4 +1,3 @@
-
 # atributos
 # marca
 # Modelo
@@ -13,27 +12,62 @@ entonces que quieres hacer?
 [4] Salir
 """)
 class Auto:
-   def __int__(self,):
+   def __init__(self):
       self.catalogo = []
 
-   def menu(self):
-      value = int(input())
+   def menu1(self,valor):
+      valor = int(input())
       while True:
-         match value:
+         match valor:
             case 1:
-               menu2()
+               self.menu2()
             case 2:
-               pass
+               print("Elige el modelo a continuacion")
+               for clave in self.catalogo():
+                  print(list(self.catalogo))
+               key = input()
+               if key in self.catalogo():
+                  nuevoValor = input("Ahora añade el nuevo modelo")
+                  self.catalogo.append({key: nuevoValor})
+               else:
+                  try:
+                     self.catalogo[None]
+                  except None:
+                     print("No hay datos para cambiar desea añadir?")
+                  except ValueError:
+                     print("No hay datos para cambiar deseas añadir?")
+               
             case 3:
                pass
+            case 4:
+               print("Saliendo...")
+               break
 
    def menu2(self):
-      print
+      print("""
+            Elije que parametro deseas cambiar?
+            [1] Añadir marca
+            [2] Añadir modelo
+            [3] Añadir año
+            """
+            )
+      j = int(input())
+      match j:
+         case 1:
+            self.marca()
+         case 2:
+            self.modelo()
+         case 3:
+            self.anio()
+
    def modelo(self):
-      catalogo[modelo] = input()
+      modelo = input("Ingrese el modelo: ")
+      self.catalogo.append({"modelo": modelo})
 
    def marca(self):
-      catalogo.update
+      marca = input("Ingrese la marca: ")
+      self.catalogo.append({"marca": marca})
 
    def anio(self):
-      anio.update
+      anio = input("Ingrese el año: ")
+      self.catalogo.append({"anio": anio})
--- Programacion II/FuncionesRecursivas/main.py
+++ Programacion II/FuncionesRecursivas/main.py
@@ -1,11 +1,13 @@
 # crear una lista ordenada de autos proporcionada por el usuario\n
 # Los datos del auto se van a meter en diccionarios especificos
-# crear una funcion para que add a el diccionario los datos porporcionados del usuario
+# crear una funcion para que add a el diccionario los datos porporcionados del usuario import Menu
+# ß
 from clase import Auto, darBienvenida
 
 catalogo=[]
 
 darBienvenida()
-
+valor = Auto()
+valor.menu1(1)
 
 
