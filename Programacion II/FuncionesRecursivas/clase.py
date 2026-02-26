3e1cf6c269f26d1331c60af36c76d518bdbe7b3a
KricoFR <kristophex5@gmail.com>
2026-02-26 10:12:17 -0700

    actualizacion de ficheros clases, main. y creacion de archivo diccionario adicional

--- Programacion II/FuncionesRecursivas/clase.py
+++ Programacion II/FuncionesRecursivas/clase.py
@@ -3,6 +3,8 @@
 # Modelo
 # anio
 
+from diccionario import catalogo
+
 def darBienvenida():
    print("""Hola nuevo usuario nos vemos de nuevo
 entonces que quieres hacer?
@@ -11,46 +13,49 @@ entonces que quieres hacer?
 [3] Solicitar un dato
 [4] Salir
 """)
+
 class Auto:
    def __init__(self):
       self.catalogo = []
 
    def menu1(self,valor):
-      valor = int(input())
       while True:
+         valor = int(input())
          match valor:
-            case 1:
+            case 1:  # Enseniar el nuevo menu para adjuntar un vehiculo
                self.menu2()
-            case 2:
+            case 2:  # Cambiar los datos de algun vehiculo ya existente, o acudir a las funcinones si no existe
                print("Elige el modelo a continuacion")
                for clave in self.catalogo():
                   print(list(self.catalogo))
                key = input()
                if key in self.catalogo():
                   nuevoValor = input("Ahora añade el nuevo modelo")
-                  self.catalogo.append({key: nuevoValor})
-               else:
-                  try:
-                     self.catalogo[None]
-                  except None:
+                  if nuevoValor == '':
                      print("No hay datos para cambiar desea añadir?")
-                  except ValueError:
-                     print("No hay datos para cambiar deseas añadir?")
+               else:
+                  self.catalogo.append({key: nuevoValor})
+               #    try:
+               #       self.catalogo[None]
+               #    except :
+               #       print("No hay datos para cambiar desea añadir?")
+               #    except ValueError:
+               #       print("No hay datos para cambiar deseas añadir?")
                
-            case 3:
+            case 3:  # Solicitar un dato dentro del diccionario
                pass
-            case 4:
+            case 4:  # Salir del menu
                print("Saliendo...")
                break
+      darBienvenida()
 
    def menu2(self):
-      print("""
-            Elije que parametro deseas cambiar?
-            [1] Añadir marca
-            [2] Añadir modelo
-            [3] Añadir año
-            """
-            )
+      print("""Elije que parametro deseas cambiar?
+[1] Añadir marca
+[2] Añadir modelo
+[3] Añadir año
+"""
+)
       j = int(input())
       match j:
          case 1:
---
+++ Programacion II/FuncionesRecursivas/diccionario.py
@@ -0,0 +1,23 @@
+catalogo = {
+
+"marca" : "Ford",
+"modelo" : "F-150",
+"anio" : "2002",
+
+"marca" : "Toyota",
+"modelo" : "Corolla",
+"anio" : "1999",
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
