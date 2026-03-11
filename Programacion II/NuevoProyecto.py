905b937a039a07a93117cd840fbe1e96cd49262e
KricoFR <kristophex5@gmail.com>
2026-03-11 17:24:40 +0000

    Adicion de documentos para la tarea Mie 11/03
    nuevos documentos para la tarea en equipo de programacion de 3 integrantes para la determinacion de un plogono cuadrangular

---
+++ Programacion II/NuevoProyecto.py
@@ -0,0 +1,22 @@
+# Crear una clase figura manejando 4 puntos pedir al usuario o general al azar
+# [-10, 10] solo enteros
+# 
+# crear una funcion de prueba
+#
+#
+from funciones import Figura, darBienvenida
+
+bienvenida = darBienvenida()
+
+print(bienvenida)
+
+
+
+
+
+
+
+
+
+
+
