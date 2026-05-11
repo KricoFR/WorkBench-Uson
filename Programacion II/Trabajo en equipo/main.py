85336f9da39573c83235290ff449e8b192bfee6f
KricoFR <kristophex5@gmail.com>
2026-05-11 17:46:26 +0000

    Add files via upload

---
+++ Programacion II/Trabajo en equipo/main.py
@@ -0,0 +1,35 @@
+# Realizar un programa para demostrar el manejo de listas en python 
+# Insertar el nuevo estudiante
+# Eliminar estudiante existente
+# Modificar algun estudiante
+"""
+Requisitos:
+=la seleccion de la accion debera realizarse de forma aleatoria
+=Este proceso debe repetirse 9 veces
+=se vebe contar con una lista de nombres alternado entre hombres y mujeres
+=Para la adicion de nuevos nombres se debe considerar:
+	-El nombre debera obtenerse de la lista de nombres disponibles
+	-El nombre agregado no debera repetirse dentro de la lista actual 
+	 de estudiantes
+=Para eliminar un estudiante
+	-El programa debera seleccionar aleatoriamente un estudiante
+	-Antes de eliminar debe validar que la lista no este vacia
+=Para modificar un estudiante
+	-El programa debera seleccionar aleatoriamente un estudiante
+	-El nuevo nombre debera tomarse de la lista de nombres disponible 
+	 y no repetirse
+=El programa debe incluir las siguientes funciones
+	-monstrar lista
+	-insertar en lista
+	-eliminar estudiante
+	-modificar estudiante
+	-funcion que controla las iteraciones y selecciones aleatorias
+=El programa debe mostrar claramente:
+	-La accion realizada
+	-El estudiante agregado eliminado o modificado
+	-lista actualizada despues de cada operacion
+	-elementos que debe incluir el programa:
+	 random, append(), insert(), remove(), pop(), manejo de indices, 
+	 if, for, while, len, print
+
+"""
